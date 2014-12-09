2f8fc23f25d327de5a9f034cda778bd8bbdf44d4
Administrateur <Administrateur@MW389>
2014-12-09 10:59:04 +0100

    First Checkin

--- public_html/app.js
+++ public_html/app.js
@@ -22,28 +22,44 @@ var valider = function () {
     $("youBirthDateError").innerHTML = "";
     
     if ($("yourName").value === '') {
+        $("yourName").style.backgroundColor = 'red';
         $("yourNameError").innerHTML = "Merci de remplir le nom !";        
         result = false;
+    } else {
+        $("yourName").style.backgroundColor = 'white';
     }
     if ($("yourFirstName").value === '') {
+        $("yourFirstName").style.backgroundColor = 'red';
         $("yourFirstNameError").innerHTML = "Merci de remplir le prénom !";
         result = false;
+    } else {
+        $("yourFirstName").style.backgroundColor = 'white';
     }
     if ($("yourNickName").value === '') {
+        $("yourNickName").style.backgroundColor = 'red';
         $("yourNickNameError").innerHTML = "Merci de remplir le pseudo !";
         result = false;
+    } else {
+        $("yourNickName").style.backgroundColor = 'white';
     }
     if ($("yourPhone").value === '') {
+        $("yourPhone").style.backgroundColor = 'red';
         $("yourPhoneError").innerHTML = "Merci de remplir le numéro de téléphone !";
         result = false;
     } else if (! telephoneRegexp.test($("yourPhone").value)) {
+        $("yourPhone").style.backgroundColor = 'orange';
         $("yourPhoneError").innerHTML = "10 charactères numeriques";
         result = false;
+    } else {
+        $("yourPhone").style.backgroundColor = 'white';
     }
     
     if ($("youBirthDate").value === '') {
+        $("youBirthDate").style.backgroundColor = 'red';
         $("youBirthDateError").innerHTML = "Merci de remplir une date de naissance";
         result = false;
+    } else {
+        $("youBirthDate").style.backgroundColor = 'white';
     }
     
     return result;
